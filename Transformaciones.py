150e4f1fee211227645b50d5f4e2f7ad1a6bd19d
Jesús García <jgarcia3@unisangil.edu.co>
2025-08-20 15:58:56 -0500

    Trnsformaciones de variables y metodo de impresion

---
+++ Transformaciones.py
@@ -0,0 +1,10 @@
+#Solicitar el nombre al usuario
+nombre = input("Digite su nombre:  ")
+print(type(nombre))
+cedula = int(input("Digite su cédula:  "))
+print(type(cedula))
+peso = float(input("Digite su peso:  "))
+print(type(peso))
+estado_matricula = bool(input("Digite True si está matriculado o False si no lo está:  "))
+print(type(estado_matricula))
+print(f"Mi nombre es {nombre}, mi cédula es {cedula} y mi peso es {peso} kg y mi estado de matrícula es {estado_matricula}")
