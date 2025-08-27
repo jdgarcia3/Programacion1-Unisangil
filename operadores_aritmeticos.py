e5a69f0ff86e1532361a643d9624f8661fc568fe
Jesús García <jgarcia3@unisangil.edu.co>
2025-08-27 15:41:46 -0500

    Se agregaron mas ejemplos

--- operadores_aritmeticos.py
+++ operadores_aritmeticos.py
@@ -1,4 +1,18 @@
 #Operadores Aritméticos
+#Libreria
+import math
 #Ejemplo 1
-a = 10 +2 (-3 * ((7 - 2) + 7 * (3 - 2)))
+a = 10 + 2 * (-3 * ((7 - 2) + 7 * (3 - 2)))
 print(a)
+#Ejemplo 2
+b = 2 * (4 * (7 + 4 * (5 * 3 - 9)) - 3 * (40 - 8))
+print(f"Ejemplo 2 = {b}")
+#Ejemplo 3
+c = 3 * (4 + (2 * (3 + 5))) + 2
+print(f"Ejemplo 3 = {c}")
+#Ejemplo 4
+d = 2 * (4 * (7 + 4 * 6) - 3 * 32)
+print(f"Ejemplo 4 = {d}")
+#Ejemplo 5
+e = (5 * (math.sqrt(81) - (math.pow(3,2) - 1))) - (42/7 + ((math.pow(10,2)/5) - 15/3)) + (2/3 * ((3 ** 2)** 2))
+print(f"Ejemplo 5 = {e}")
