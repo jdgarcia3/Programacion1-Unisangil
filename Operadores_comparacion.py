48b865f9da1325cc47bd59603687001e74063ef1
Jesús García <jgarcia3@unisangil.edu.co>
2025-09-03 14:54:58 -0500

    Operadores de comparacion

---
+++ Operadores_comparacion.py
@@ -0,0 +1,15 @@
+#Operadores de comparacion
+num1 = 10
+num2 = 10
+#Igualdad
+print(num1 == num2) 
+#Diferencia
+print(num1 != num2)
+#Mayor que
+print(num1 > num2)
+#Menor que
+print(num1 < num2)
+#Mayor o igual que
+print(num1 >= num2)
+#Menor o igual que
+print(num1 <= num2)
