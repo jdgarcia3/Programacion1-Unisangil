3e31456cddc8f7889f7deccc81258b4a5cc9b986
Jesús García <jgarcia3@unisangil.edu.co>
2025-09-10 14:26:03 -0500

    Estructura de control if-else-elif

---
+++ estructura_if.py
@@ -0,0 +1,23 @@
+#Estructura de control : if
+numero = int(input("Ingrese un número: "))
+#if
+if numero > 0:
+    print("El número es positivo")
+# if - else
+if numero > 0:
+    print("El número es positivo")
+else:
+    print("El número es negativo")
+#if-elif-else
+if numero == 1:
+    print("Número 1")
+elif numero == 2:
+    print("Número 2")
+elif numero == 3:
+    print("Número 3")
+elif numero == 4:
+    print("Número 4")
+elif numero == 5:
+    print("Número 5")
+else:
+    print("Número desconocido")
