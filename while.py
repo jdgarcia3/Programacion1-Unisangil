ad2d51e620f7e71be68a68d9153475e00683565c
Jesús García <jgarcia3@unisangil.edu.co>
2025-09-10 14:55:54 -0500

    Estrutura de repetición while con condional

---
+++ while.py
@@ -0,0 +1,15 @@
+#Estructura de repetición while
+"""
+op = int(input("Ingrese una opción: "))
+while op != 0:
+    print("Unisangil")
+print("Fuera del buble")
+"""
+#While True
+while True:
+    numero = int(input("Ingrese un número: "))
+    if numero == 0:
+        break
+    else:
+        print(f"Error!!!")
+print("Fuera del buble")
