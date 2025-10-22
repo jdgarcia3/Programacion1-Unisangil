e7c229f43f5cba46dcdd18777404c8533441c43e
Jesús García <jgarcia3@unisangil.edu.co>
2025-10-22 14:56:34 -0500

    diccionarios

---
+++ diccionario.py
@@ -0,0 +1,37 @@
+#Crear un diccionario
+persona = {}
+# Diccionario frutas
+Programacion_1 = {
+    "docente": "David Garcia",
+    "estudiantes":[
+        {
+            "nombre": "Juan Gonzalez",
+            "cedula": 1015434287,
+            "correo": "juango@unisangil.edu.co",
+            "edad": 20
+        },
+        {
+            "nombre": "Camila Suarez",
+            "cedula": 1015685432,
+            "correo": "camilasu@unisangil.edu.co",
+            "edad": 23
+        }
+    ],
+    "temas": [
+        "Introducción a la programación",
+        "Variables",
+        "Estructuras de control",
+        "Programación orientada a objetos"
+    ],
+    "creditos": 3,
+    "cortes":[
+        {
+            "nombre": "Corte 1",
+            "porcentaje": 50
+        },
+        {
+            "nombre": "Corte 2",
+            "porcentaje": 50
+        }
+    ]
+}
